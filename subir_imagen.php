c28d9e4f63918717600a8b3fa212dbdc180c1f9a
jeneque <30362859+jeneque@users.noreply.github.com>
2017-07-22 01:20:47 -0500

    Update subir_imagen.php

--- subir_imagen.php
+++ subir_imagen.php
@@ -1,4 +1,5 @@
-<?php
+/*PARA VERSIONES ANTERIORES DE PHP*/
+/*<?php
 $mi_usuario='root';
 $mi_password='mispassword';
 
@@ -30,6 +31,51 @@ if(!is_writable($dir_destino)){
 			echo "<p>$codigo</p>";
 			echo "<p>$descripcion</p>";
 			echo "<img src='http://localhost/miweb/uploads/". basename($imagen_subida) ."' />";
+		} else {
+			echo "Posible ataque de carga de archivos!\n";
+		}
+	}else{
+		echo "Posible ataque del archivo subido: ";
+		echo "nombre del archivo '". $_FILES['archivo_usuario']['tmp_name'] . "'.";
+	}
+}
+
+?>
+*/
+/*PARA NUEVAS VERSIONES DE PHP*/
+<?php
+$mi_usuario='root';
+$mi_password='tupassword';
+
+$dir_destino = 'imagenes/';
+$imagen_subida = $dir_destino . basename($_FILES['foto']['name']);
+
+//Variables del metodo POST
+$codigo=$_POST['codigo'];
+$descripcion=$_POST['descripcion'];
+
+if(!is_writable($dir_destino)){
+	echo "no tiene permisos";
+}else{
+	if(is_uploaded_file($_FILES['foto']['tmp_name'])){
+		/*echo "Archivo ". $_FILES['foto']['name'] ." subido con éxtio.\n";
+		echo "Mostrar contenido\n";
+		echo $imagen_subida;*/
+		if (move_uploaded_file($_FILES['foto']['tmp_name'], $imagen_subida)) {
+
+			$link = mysqli_connect('localhost', $mi_usuario, $mi_password)
+				or die('Uyy!!!: ' . mysqli_error());
+			mysqli_select_db($link,'test') or die('No pudo selecionar la BD');
+			//Creamos nuestra consulta sql
+			$query="insert into producto(codigo, descripcion, imagen) value ('$codigo', '$descripcion', '$imagen_subida')";
+			//Ejecutamos la consutla
+			mysqli_query($link,$query) or die('Error al procesar consulta: ' . mysqli_error());
+
+			echo "El archivo es fue cargado exitosamente.\n";
+
+			echo "<p>$codigo</p>";
+			echo "<p>$descripcion</p>";
+			echo "<img src='imagenes/". basename($imagen_subida) ."' />";
 		} else {
 			echo "Posible ataque de carga de archivos!\n";
 		}
